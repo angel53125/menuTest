0d5fc3b001a0ab7b17b9069ef921184a1fa8bd61
Angel Negron <angel53125@hotmail.com>
2022-12-14 18:20:00 -0500

    test

--- app/src/main/java/com/hfad/myapplication/MainActivity.java
+++ app/src/main/java/com/hfad/myapplication/MainActivity.java
@@ -1,10 +1,13 @@
 package com.hfad.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,5 +28,19 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflator = getMenuInflater();
         inflator.inflate(R.menu.horoscope_menu,menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.itemTest:
+                Toast.makeText(this,"Item 1 selected",Toast.LENGTH_LONG);
+            case R.id.itemTest2:
+                Toast.makeText(this,"Item 2 selected",Toast.LENGTH_LONG);
+            case R.id.itemTest3:
+                Toast.makeText(this,"Item 3 selected",Toast.LENGTH_LONG);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
